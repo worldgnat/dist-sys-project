3065bb023910b34c6a206380aa95b14e5c42a297
Peter Davoust <peter@peterdavoust.com>
2013-11-06 11:04:44 -0500

    Updated ClientRunner so it will calculate and print the average time returned by the clients.

--- D1ClientSrc/ClientRunner.java
+++ D1ClientSrc/ClientRunner.java
@@ -7,8 +7,10 @@ import java.util.concurrent.LinkedBlockingQueue;
 public class ClientRunner {
 	public void main(String[] args) {
 		LinkedBlockingQueue<Long> responseTimes = new LinkedBlockingQueue<Long>();
+		
 		ArrayList<client> clients = new ArrayList<client>();
 		ArrayList<String> commands;
+		
 		try {
 			for (String file : args) {
 				commands = new ArrayList<String>();
@@ -25,8 +27,8 @@ public class ClientRunner {
 				new Thread(temp).start();
 				in.close();
 			}
-			//Add a shutdown hook to make sure we killall the clients on ctrl+c
-			Runtime.getRuntime().addShutdownHook(new Thread(new ClientKiller(clients)));
+			//Add a shutdown hook to make sure we killall the clients and print response time on ctrl+c
+			Runtime.getRuntime().addShutdownHook(new Thread(new ClientKiller(responseTimes, clients)));
 		}
 		catch (Exception er) {
 			er.printStackTrace();
@@ -36,13 +38,25 @@ public class ClientRunner {
 
 class ClientKiller implements Runnable {
 	ArrayList<client> clients;
-	public ClientKiller(ArrayList<client> clients) {
+	LinkedBlockingQueue<Long> responseTimes;
+	public ClientKiller(LinkedBlockingQueue<Long> responseTimes, ArrayList<client> clients) {
 		this.clients = clients;
+		this.responseTimes = responseTimes;
 	}
 	
 	public void run() {
+		//Kill all running auto-clients
 		for (client client : clients) {
 			client.stop();
 		}
+		
+		//Calculate the average response time for this run and print it
+		long avgTime = 0;
+		int numSamples = responseTimes.size();
+		for (long time : responseTimes) {
+			avgTime += time;
+		}
+		avgTime = avgTime / numSamples;
+		System.out.println("Average Response Time: " + avgTime);
 	}
 }
