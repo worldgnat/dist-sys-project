534fad0fadc5622e2d2c75398a3ffea8f3c7d72a
Peter Davoust <peter@peterdavoust.com>
2013-11-07 17:52:19 -0500

    Fixed a /profoundly/ dumb mistake in ClientRunner. Seriously: don't even look at it; it's embarrassing.

--- D1ClientSrc/ClientRunner.java
+++ D1ClientSrc/ClientRunner.java
@@ -5,7 +5,7 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 
 public class ClientRunner {
-	public void main(String[] args) {
+	public static void main(String[] args) {
 		LinkedBlockingQueue<Long> responseTimes = new LinkedBlockingQueue<Long>();
 		
 		ArrayList<client> clients = new ArrayList<client>();
