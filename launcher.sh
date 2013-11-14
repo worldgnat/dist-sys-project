771ecc4ef020748ddb92ec106be101bd49ba8eb4
Peter Davoust <peter@peterdavoust.com>
2013-11-14 13:25:57 -0500

    Updated launcher to reflect changes in Middleware.

--- launcher.sh
+++ launcher.sh
@@ -2,8 +2,8 @@
 
 export CLASSPATH=$(pwd)/D1ServerSrc
 rmiregistry $1 &
-if [ $# -ge 2 ]; then 
+if [ $# -eq 2 ]; then 
 	java -Djava.security.policy=D1ServerSrc/server.policy -Djava.rmi.server.codebase=$(pwd)/D1ServerSrc ResImpl.ResourceManagerImpl $1 $2
-else
-	java -Djava.security.policy=D1ServerSrc/server.policy -Djaja.rmi.server.codebase=$(pwd)/D1ServerSrc ResImpl.Middleware localhost $1
+elif [ $# -eq 4 ]; then 
+	java -Djava.security.policy=D1ServerSrc/server.policy -Djaja.rmi.server.codebase=$(pwd)/D1ServerSrc ResImpl.Middleware $1 $2 $3 $4
 fi 
