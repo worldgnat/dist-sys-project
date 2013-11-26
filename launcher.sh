0edbae8c63486ecd3b6ad8fab5c205b04e124f2c
Peter Davoust <peter.davoust@mail.mcgill.ca>
2013-11-26 14:47:30 -0500

    Updated launcher script so it adds command-line arguments for JGroups.

--- launcher.sh
+++ launcher.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-export CLASSPATH=$(pwd)/D1ServerSrc
+export CLASSPATH=$(pwd)/D1ServerSrc:$(pwd)/jgroups-3.4.1.Final.jar
 killall -u $(whoami) rmiregistry
 IP=$(ifconfig | grep eth0 -A 10 | grep "inet addr:" | sed -e 's/[^:]*:\([^ ]*\).*/\1/')
+LOGGING="-Djava.util.logging.config.file=../log4j.properties"
 rmiregistry $1 &
 if [ $# -eq 2 ]; then 
-	java -Djava.security.policy=D1ServerSrc/server.policy -Djava.net.preferIPv4Stack=true -Djgroups.bind_addr=$IP$IP$IP$IP -Djava.rmi.server.codebase=$(pwd)/D1ServerSrc ResImpl.ResourceManagerImpl $1 $2
+	java -Djava.security.policy=D1ServerSrc/server.policy -Djava.net.preferIPv4Stack=true -Djgroups.bind_addr=$IP -Djava.rmi.server.codebase=$(pwd)/D1ServerSrc $LOGGING ResImpl.ResourceManagerImpl $1 $2
 elif [ $# -eq 4 ]; then 
 	java -Djava.security.policy=D1ServerSrc/server.policy -Djaja.rmi.server.codebase=$(pwd)/D1ServerSrc ResImpl.Middleware $1 $2 $3 $4
 fi 
