88637c5f1ba373070225c156a880dcb4b796d458
Peter Davoust <peter@peterdavoust.com>
2013-11-27 13:51:31 -0500

    More goats.

--- D1ServerSrc/ResImpl/GroupManagement.java
+++ D1ServerSrc/ResImpl/GroupManagement.java
@@ -61,7 +61,8 @@ public class GroupManagement extends ReceiverAdapter {
 	 */
     public void viewAccepted(View new_view) {
         System.out.println("[GM - INFO] New view: " + new_view);
-        List<Address> members = new ArrayList<Address>(new_view.getMembers());
+        List<Address> members = new ArrayList<Address>();
+        members.addAll(new_view.getMembers());
         //The member with the lowest number is, arbitrarily, the primary copy
         Collections.sort(members);
         if (channel.getAddress().equals(members.get(0))) {
