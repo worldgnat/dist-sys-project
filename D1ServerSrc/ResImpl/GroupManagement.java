bf946b0b67fde9224dc75686808365c1867d1358
Peter Davoust <peter@peterdavoust.com>
2013-11-27 13:47:40 -0500

    Crossing fingers didn't work. Sacrificed a goat instead.

--- D1ServerSrc/ResImpl/GroupManagement.java
+++ D1ServerSrc/ResImpl/GroupManagement.java
@@ -10,6 +10,7 @@ import groupComm.RequestPrimary;
 import groupComm.StartMessage;
 
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -60,8 +61,7 @@ public class GroupManagement extends ReceiverAdapter {
 	 */
     public void viewAccepted(View new_view) {
         System.out.println("[GM - INFO] New view: " + new_view);
-        List<Address> members = new LinkedList<Address>();
-        members.addAll(new_view.getMembers());
+        List<Address> members = new ArrayList<Address>(new_view.getMembers());
         //The member with the lowest number is, arbitrarily, the primary copy
         Collections.sort(members);
         if (channel.getAddress().equals(members.get(0))) {
