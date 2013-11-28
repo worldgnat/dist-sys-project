160f7d5fbefa71af7ef4bd03bc0dda6f6d4be43d
Peter Davoust <peter@peterdavoust.com>
2013-11-28 00:39:35 -0500

    The reason JGroups never complained about my messages not implementing serializable is that I never sent any of them... fail.

--- D1ServerSrc/groupComm/RMMessage.java
+++ D1ServerSrc/groupComm/RMMessage.java
@@ -1,10 +1,12 @@
 package groupComm;
 
-public class RMMessage {
+import java.io.Serializable;
+
+public class RMMessage implements Serializable {
 	int tid;
 	String sourceChannel;
 	
-	public RMMessage(int tid, String sourceChannel) {
+	public RMMessage(int tid, String sourceChannel){
 		this.tid = tid;
 		this.sourceChannel = sourceChannel;
 	}
