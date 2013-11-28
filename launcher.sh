5a25ede05bb7149caf9a3cc6aea017a87e05b034
Peter Davoust <peter.davoust@mail.mcgill.ca>
2013-11-28 10:29:14 -0500

    Changed launcher.sh so it uses the official 3.4.1 final release of JGroups - we don't want to be using alpha code in our project

--- launcher.sh
+++ launcher.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-export CLASSPATH=$(pwd)/D1ServerSrc:$(pwd)/jgroups-3.5.0.Alpha1.jar
+export CLASSPATH=$(pwd)/D1ServerSrc:$(pwd)/jgroups-3.4.1.Final.jar
 killall -u $(whoami) rmiregistry && sleep 1
 IP=$(ifconfig | grep eth0 -A 10 | grep "inet addr:" | sed -e 's/[^:]*:\([^ ]*\).*/\1/')
 #LOGGING="-Djava.util.logging.config.file=logger.properties"
-LOGGING="-Djgroups.udp.ip_ttl=128"
+JAVA_OPTS="-Djava.net.preferIPv4Stack=true -Djgroups.bind_addr=$IP -Djava.rmi.server.codebase=file:$(pwd)/D1ServerSrc"
 rmiregistry $1 &
 if [ $# -eq 2 ]; then 
-	java -Djava.security.policy=D1ServerSrc/server.policy -Djava.net.preferIPv4Stack=true -Djgroups.bind_addr=$IP -Djava.rmi.server.codebase=$(pwd)/D1ServerSrc $LOGGING ResImpl.ResourceManagerImpl $1 $2
+#	if [ $2 == "cars29" ]; then
+#		JAVA_OPTS=$JAVA_OPTS" -Djgroups.udp.mcast_port"
+#	elif [ $2 == "rooms29" ]; then 
+#		JAVA_OPTS=$JAVA_OPTS" -Djgroups.udp.mcast_port"
+#	else
+#		JAVA_OPTS=
+	java -Djava.security.policy=D1ServerSrc/server.policy $JAVA_OPTS ResImpl.ResourceManagerImpl $1 $2
 elif [ $# -eq 1 ]; then 
-	java -Djava.security.policy=D1ServerSrc/server.policy -Djava.rmi.server.codebase=$(pwd)/D1ServerSrc ResImpl.Middleware $1 
+	java -Djava.security.policy=D1ServerSrc/server.policy $JAVA_OPTS ResImpl.Middleware $1 
 fi 
